431d1c6d464c4ae2eb32829e59df032877e7e3e4
ipesoa <i.pesoa@protonmail.com>
2026-09-16 15:26:05 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -9,5 +9,5 @@ window.APP_CONFIG = {
   version: "0.4.2",
   lastReview: "2026-09-16T14:35:00+02:00",
   creatorName: "iPesoa editorial",
-  creatorUrl: "https://www.instagram.com/ipesoa/"
+  creatorUrl: "https://www.instagram.com/ipesoaeditorial/"
 };
